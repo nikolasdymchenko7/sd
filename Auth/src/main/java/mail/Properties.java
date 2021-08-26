5257013605541efb8405d0858a12e76f27082c49
nicolasdymchenko7 <nicolasdymchenko@gmail.com>
2021-08-26 16:46:27 +0300

    Change class name Properties

--- Auth/src/main/java/mail/Properties.java
+++ Auth/src/main/java/mail/Properties.java
@@ -6,10 +6,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-public class JavaProperties {
+public class Properties {
     public String getProperty(String property) throws IOException {
         InputStream input = PropertyReader.class.getClassLoader().getResourceAsStream("config.properties");
-        Properties prop = new Properties();
+        java.util.Properties prop = new java.util.Properties();
         prop.load(input);
         return prop.getProperty(property);
     }
